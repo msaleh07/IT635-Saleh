3ea93091b6ab0d51f5da34e2a305c1da3e210f08
msaleh07 <99453633+msaleh07@users.noreply.github.com>
2022-03-16 21:59:27 -0400

    Add files via upload

---
+++ location.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+conn = psycopg2.connect("postgresql://network_admin:saleh@localhost/network_inventory")
+cur = conn.cursor()
+
+print("Please enter device ID")
+device_id = int(input())
+cur.execute("""
+    SELECT sites.name, devices.product_des, tech.name 
+    FROM sites, tech, devices
+    WHERE devices.device_id=sites.site_id
+          AND device_id=%s
+""", (device_id,));     
+for row in cur:
+    product_des = row[1]
+    tech_name = row[2]
+    print(f"{product_des} , {tech_name}")
+print(f"Installed at: {row[0]}")
+
+cur.close()
+conn.close()
